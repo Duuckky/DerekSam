f899da04f53fcb362945cfd52e10610c57f69a65
Duuckky <Sam.duuckk@gmail.com>
2014-06-05 19:38:02 -0400

    Added setter + getter

--- DerekSam/src/me/dereksam/main/Main.java
+++ DerekSam/src/me/dereksam/main/Main.java
@@ -2,13 +2,21 @@ package me.dereksam.main;
 
 public class Main {
 	
+	private static String name;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-<<<<<<< HEAD
 		System.out.println("Sam has no friends");
-=======
 		System.out.println("derek ur a fgt");
->>>>>>> 0d9b965e65403372ce20433e59cda6011d1807c6
+		
+		setName("Derek");
+		System.out.println(getName());
 	}
+	
+	public static void setName(String x) {
+		name = x;
+	}
+	
+	public static String getName() { return name; }
 }
 
