864eddbc3566d7f2ac6b26e9611318fc4efc3df5
Duuckky <Sam.duuckk@gmail.com>
2014-06-05 19:56:01 -0400

    RESET
    WOW

--- DerekSam/src/me/dereksam/main/Main.java
+++ DerekSam/src/me/dereksam/main/Main.java
@@ -2,34 +2,17 @@ package me.dereksam.main;
 
 public class Main {
 	
-	private static String name;
-	private static int age;
-	private static String lastName;
+	/*
+	 * TODO Inventory
+	 * TODO Race
+	 * TODO Class
+	 * TODO Story
+	 * TODO Character Creation
+	 * TODO Name
+	 */
 	
 	public static void main(String[] args) {
-		age = 15;
-		System.out.println("Hello, World!");
-		System.out.println("Sam has no friends");
-		System.out.println("derek ur a fgt");
-		System.out.println("I am currently " +age);
-		age += 1;
-		System.out.println("In 1 year I will be " +age);
 		
-		setName("Derek");
-		setLastName("Marshall");
-		System.out.println(getFullName());
 	}
-	
-	public static void setName(String x) {
-		name = x;
-	}
-	
-	public static void setLastName(String x) {
-		lastName = x;
-	}
-
-	public static String getName() { return name; }
-	public static String getLastName() { return lastName; }
-	public static String getFullName() { return name + " " + lastName; }
 }
 
