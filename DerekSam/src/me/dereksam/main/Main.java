9284a66c7b4d8d784b98b4774d490bbd660acb36
RoadRunner8579 <roadrunner.8@hotmail.com>
2014-06-05 19:50:00 -0400

    Added an int with an adder
    My sick skills are sick

--- DerekSam/src/me/dereksam/main/Main.java
+++ DerekSam/src/me/dereksam/main/Main.java
@@ -3,11 +3,16 @@ package me.dereksam.main;
 public class Main {
 	
 	private static String name;
+	private static int age;
 	
 	public static void main(String[] args) {
+		age = 15;
 		System.out.println("Hello, World!");
 		System.out.println("Sam has no friends");
 		System.out.println("derek ur a fgt");
+		System.out.println("I am currently " +age);
+		age += 1;
+		System.out.println("In 1 year I will be " +age);
 		
 		setName("Derek");
 		System.out.println(getName());
