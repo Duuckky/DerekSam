883126b13a0e17c2ab57e9a1cca1ecd8431e1ea2
Duuckky <Sam.duuckk@gmail.com>
2014-06-05 19:45:44 -0400

    Added last and full name

--- DerekSam/src/me/dereksam/main/Main.java
+++ DerekSam/src/me/dereksam/main/Main.java
@@ -3,6 +3,7 @@ package me.dereksam.main;
 public class Main {
 	
 	private static String name;
+	private static String lastName;
 	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
@@ -10,13 +11,20 @@ public class Main {
 		System.out.println("derek ur a fgt");
 		
 		setName("Derek");
-		System.out.println(getName());
+		setLastName("Marshall");
+		System.out.println(getFullName());
 	}
 	
 	public static void setName(String x) {
 		name = x;
 	}
 	
+	public static void setLastName(String x) {
+		lastName = x;
+	}
+
 	public static String getName() { return name; }
+	public static String getLastName() { return lastName; }
+	public static String getFullName() { return name + " " + lastName; }
 }
 
